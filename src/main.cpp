6692a70bd624e9ea428080b909f68e5315779538
Minjun Lee <lmjg0407@gmail.com>
2026-05-07 21:18:09 -0400

    feat: applying the numbers

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,12 @@ Trade createTrade(const Order& buyOrder, const Order& sellOrder) {
     };
 }
 
+// Because we modify, we do not use const infront of the parameters
+void applyTrade(Order& buyOrder, Order& sellOrder, const Trade& trade) {
+    buyOrder.quantity -= trade.quantity;
+    sellOrder.quantity -= trade.quantity;
+}
+
 void printOrder(const Order& order) {
     std::cout << "Order ID: " << order.id << '\n';
     std::cout << "Side: " << sideToString(order.side) << '\n';
@@ -74,21 +80,28 @@ int main() {
     std::vector<Order> orders;
 
     Order sellOrder(Order{1, Side::Sell, 100, 50});
-    Order buyOrder(Order{2, Side::Buy, 101, 20});
+    Order buyOrder{2, Side::Buy, 101, 80};
+    // Order buyOrder(Order{2, Side::Buy, 101, 20});
+    // Order buyOrder{2, Side::Buy, 99, 20};
     
-
-    std::cout << "Sell Order:" << '\n';
+    std::cout << "Before matching" << '\n';
     printOrder(sellOrder);
-
-    std::cout << "Buy Order:" << '\n';
     printOrder(buyOrder);
 
     if (canMatch(buyOrder, sellOrder)) {
         Trade trade = createTrade(buyOrder, sellOrder);
+        applyTrade(buyOrder, sellOrder, trade);
+
+        std::cout << "Trade happened:" << '\n';
         printTrade(trade);
     } else {
         std::cout << "No trade happened." << '\n';
     }
+
+    std::cout << '\n';
+    std::cout << "After matching:" << '\n';
+    printOrder(sellOrder);
+    printOrder(buyOrder);
 
     return 0;
 }
